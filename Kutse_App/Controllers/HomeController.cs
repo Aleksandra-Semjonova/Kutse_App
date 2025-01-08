037fd6886f1147c1be298a6c38b3ba696b658705
Deniel Kruusman <denielkruusman@gmail.com>
2025-01-08 11:33:06 +0200

    Add project files.

---
+++ Kutse_App/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Kutse_App.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Ootan sind minu peole! Palun Tule!!!";
+            int hour = DateTime.Now.Hour;
+            ViewBag.Greting = hour < 10 ? "Tere hommikust!" : "Tere päevast!";
+            return View();
+        }
+
+        [HttpGet]
+
+        public ViewResult Ankeet()
+        {
+            return View();
+        }
+    }
+}
